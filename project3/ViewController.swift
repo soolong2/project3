75c9d24e3d4fa733334d44b0da4945e3a5cda4af
soo <gustn9360@naver.com>
2022-05-24 14:53:34 +0900

    Initial Commit

---
+++ project3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project3
+//
+//  Created by so on 2022/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
