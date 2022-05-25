873e2024a73f3469ee650b06a5890e80fceefc83
soo <gustn9360@naver.com>
2022-05-25 20:55:42 +0900

    처음작업

---
+++ project3/Countries.swift
@@ -0,0 +1,21 @@
+//
+//  Countries.swift
+//  project3
+//
+//  Created by so on 2022/05/25.
+//
+
+import Foundation
+//[
+//      {"korean_name":"한국","asset_name":"kr"},
+//      {"korean_name":"독일","asset_name":"de"},
+//      {"korean_name":"이탈리아","asset_name":"it"},
+//      {"korean_name":"미국","asset_name":"us"},
+//      {"korean_name":"프랑스","asset_name":"fr"},
+//      {"korean_name":"일본","asset_name":"jp"}
+//]
+
+struct Countries: Codable {
+    let korean_name: String
+    let asset_name: String
+}
--- project3/ViewController.swift
+++ project3/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    //화면클릭
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let viewController = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") else {return}
+        
+        navigationController?.pushViewController(viewController, animated: true)
+        print("😄😄😄😄😄😄😄😄\(indexPath.row)")
+    }
+    @IBOutlet weak var countrieView: UITableView!
+   
+    let cellIdentifier: String = "cell"
+    
+    var countries: [Countries] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        let con: Countries = self.countries[indexPath.row]
+        cell.textLabel?.text = con.korean_name + "(\(con.asset_name))"
+        return cell
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "countries") else {
+            return
+        }
+        do {
+            self.countries = try! jsonDecoder.decode([Countries].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        }
+        // 왜안되는거지
+        //self.tableView.reloadData()
         // Do any additional setup after loading the view.
     }
 
 
-}
+
 
